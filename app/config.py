2408309d175703b6c076a39aa816a6cdc7b34b3b
Will Cheng <chengyong@pku.edu.cn>
2024-07-28 15:58:53 +0800

    feat: add DATASET_RAW_FOLDER and TRAINING_CONFIGS_FOLDER

--- app/config.py
+++ app/config.py
@@ -1,3 +1,13 @@
+'''
+Author: Will Cheng chengyong@pku.edu.cn
+Date: 2024-07-24 16:53:06
+LastEditors: Will Cheng chengyong@pku.edu.cn
+LastEditTime: 2024-07-28 10:23:29
+FilePath: /PoseidonAI-Server/app/config.py
+Description: 
+
+Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved. 
+'''
 import os
 from datetime import timedelta
 from os.path import abspath, dirname, join
@@ -7,7 +17,10 @@ current_dir = dirname(current_file_path)
 
 class Config:
     STATIC_FOLDER = abspath(join(current_dir, os.pardir, 'data', 'static'))
+    DATASET_RAW_FOLDER = abspath(join(current_dir, os.pardir, 'data', 'dataset_raw'))
+    TRAINING_CONFIGS_FOLDER = abspath(join(current_dir, os.pardir, 'data', 'configs'))
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
     MONGO_URI = 'mongodb://admin:admin@localhost:27017/poseidon?authSource=admin&retryWrites=true&w=majority'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt_secret_key'  # JWT 签名的密钥
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # 访问令牌过期时间
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=48)  # 访问令牌过期时间
