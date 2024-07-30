8265cacd98f4f5a04e5ae4d109d6a1106cbb4a18
Will Cheng <will.cheng@efctw.com>
2024-07-30 17:16:29 +0800

    feat: register algorithms blueprint

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+'''
+Author: Will Cheng (will.cheng@efctw.com)
+Date: 2024-07-29 08:28:38
+LastEditors: Will Cheng (will.cheng@efctw.com)
+LastEditTime: 2024-07-30 09:20:48
+FilePath: /PoseidonAI-Server/run.py
+'''
 from app import create_app
 from app.celery_utils import create_celery_app
 from flask import jsonify
@@ -12,12 +19,17 @@ from routes.auth import auth_bp
 from routes.datasets import datasets_bp
 from routes.detect_types import detect_types_bp
 from routes.dataset_formats import dataset_formats_bp
+from routes.training_configurations import training_configurations_bp
+from routes.training_frameworks import training_frameworks_bp
+from routes.algorithms import algorithms_bp
 
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
 app.register_blueprint(datasets_bp, url_prefix='/api/datasets')
 app.register_blueprint(detect_types_bp, url_prefix='/api/detect-types')
 app.register_blueprint(dataset_formats_bp, url_prefix='/api/dataset-formats')
-
+app.register_blueprint(training_configurations_bp, url_prefix='/api/training-configurations')
+app.register_blueprint(training_frameworks_bp, url_prefix='/api/training-frameworks')
+app.register_blueprint(algorithms_bp, url_prefix='/api/algorithms')
 
 if __name__ == '__main__':
     app.run(debug=True)
