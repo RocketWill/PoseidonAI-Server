b6e5be8749874e12e24c2973d9d4b0f40b89ad16
Will Cheng <chengyong@pku.edu.cn>
2024-08-05 19:42:59 +0800

    chore: move remove_unannotated_images to common

--- utils/common/__init__.py
+++ utils/common/__init__.py
@@ -2,7 +2,7 @@
 Author: Will Cheng chengyong@pku.edu.cn
 Date: 2024-07-26 16:59:09
 LastEditors: Will Cheng chengyong@pku.edu.cn
-LastEditTime: 2024-08-03 21:32:00
+LastEditTime: 2024-08-05 19:24:08
 FilePath: /PoseidonAI-Server/utils/common/__init__.py
 Description: 
 
@@ -28,7 +28,27 @@ def read_json(file):
 def write_json(data, file):
     with open(file, "w") as f:
         json.dump(data, f, indent=4)
-        
+    
 def write_yaml(data, output_file):
     with open(output_file, 'w') as file:
-        yaml.dump(data, file, default_flow_style=False)
+        yaml.dump(data, file, default_flow_style=False, sort_keys=False)
+        
+def remove_unannotated_images(coco_annotation_file):
+    # 检查 coco_annotation_file 是否为字典类型
+    if not isinstance(coco_annotation_file, dict):
+        coco_data = read_json(coco_annotation_file)
+    else:
+        coco_data = coco_annotation_file
+
+    # 获取有标注的图片 ID
+    annotated_image_ids = {ann['image_id'] for ann in coco_data['annotations']}
+    
+    # 过滤有标注的图片
+    annotated_images = [img for img in coco_data['images'] if img['id'] in annotated_image_ids]
+    coco_data['images'] = annotated_images
+    
+    # 过滤没有标注的图片的标注
+    annotated_annotations = [ann for ann in coco_data['annotations'] if ann['image_id'] in annotated_image_ids]
+    coco_data['annotations'] = annotated_annotations
+    
+    return coco_data
