9687e78b3c5fb146479079ad27f66e3180b1ec78
Will Cheng <will.cheng@efctw.com>
2024-08-05 16:26:03 +0800

    chore: add training_tasks_bp

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 Author: Will Cheng (will.cheng@efctw.com)
 Date: 2024-07-29 08:28:38
 LastEditors: Will Cheng (will.cheng@efctw.com)
-LastEditTime: 2024-07-30 09:20:48
+LastEditTime: 2024-08-05 11:26:35
 FilePath: /PoseidonAI-Server/run.py
 '''
 from app import create_app
@@ -22,6 +22,7 @@ from routes.dataset_formats import dataset_formats_bp
 from routes.training_configurations import training_configurations_bp
 from routes.training_frameworks import training_frameworks_bp
 from routes.algorithms import algorithms_bp
+from routes.training_task import training_tasks_bp
 
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
 app.register_blueprint(datasets_bp, url_prefix='/api/datasets')
@@ -30,6 +31,7 @@ app.register_blueprint(dataset_formats_bp, url_prefix='/api/dataset-formats')
 app.register_blueprint(training_configurations_bp, url_prefix='/api/training-configurations')
 app.register_blueprint(training_frameworks_bp, url_prefix='/api/training-frameworks')
 app.register_blueprint(algorithms_bp, url_prefix='/api/algorithms')
+app.register_blueprint(training_tasks_bp, url_prefix='/api/training-tasks')
 
 if __name__ == '__main__':
     app.run(debug=True)
