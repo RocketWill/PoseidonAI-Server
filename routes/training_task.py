a32394b0635fba5ce6ddb2116a3e931e890ec0c7
Will Cheng <will.cheng@efctw.com>
2024-08-09 16:39:59 +0800

    feat: Add comprehensive API routes for managing training tasks
    - Implemented routes for creating, monitoring, and managing training tasks within the `training_task` module.
    - Added `/create` route to initiate training tasks, with support for specifying task details like algorithm, dataset, configuration, GPU, model, epochs, and validation ratio.
    - Developed `/task-create-status/<task_id>` route to retrieve the creation status of a specific training task.
    - Included `/list` route to list all training tasks associated with a user.
    - Created `/list/<task_id>` route to get detailed information about a specific training task, including its current state.
    - Introduced `/train/<task_id>` route to start the training process for a specific task, storing the task ID in Redis for status tracking.
    - Added `/train-status/<task_id>/<algo_name>/<framework_name>/<save_key>` route to fetch the training progress and update the task's status in the database and Redis.
    - Implemented `/train/<task_id>/<algo_name>/<framework_name>` route to stop a running training task, updating its status to "REVOKED".
    
    These routes provide a robust API for managing the lifecycle of training tasks, enabling users to create, monitor, and control tasks efficiently.

--- routes/training_task.py
+++ routes/training_task.py
@@ -2,27 +2,25 @@
 Author: Will Cheng chengyong@pku.edu.cn
 Date: 2024-07-24 22:17:36
 LastEditors: Will Cheng (will.cheng@efctw.com)
-LastEditTime: 2024-08-06 16:09:07
+LastEditTime: 2024-08-09 16:33:19
 FilePath: /PoseidonAI-Server/routes/training_task.py
 Description: 
 
 Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved. 
 '''
+
 import os
-from bson import ObjectId
-import traceback
 import uuid
-
+import traceback
 from flask import Blueprint, request, jsonify
-
-from .utils import parse_immutable_dict
+from app import redis_client
 from routes.auth import jwt_required
 from services.training_task_service import TrainingTaskService
+from .utils import parse_immutable_dict
 
 training_tasks_bp = Blueprint('training_tasks', __name__)
 
-# {'name': 'Training-Task__strong-shark', 'description': '', 'algorithm': '66a74cf1c96399a9ff514d6f', 'dataset': '66b035f2687d0c81e9845eca', 'config': '66a75082087af8b1a7a0b761', 'gpu': 0, 'model': 'yolov8n.pt', 'epoch': 30, 'trainValRatio': 0.1}
-
+# 創建訓練任務的路由
 @training_tasks_bp.route('/create', methods=['POST'])
 @jwt_required
 def create_training_task(user_id):
@@ -48,6 +46,7 @@ def create_training_task(user_id):
         traceback.print_exc()
     return jsonify(response), 200
 
+# 查詢訓練任務創建狀態的路由
 @training_tasks_bp.route('/task-create-status/<task_id>', methods=['GET'])
 def get_task_create_status(task_id):
     response = {'code': 200, 'msg': 'ok', 'show_msg': 'ok', 'results': None}
@@ -59,6 +58,7 @@ def get_task_create_status(task_id):
         response['msg'] = str(e)
     return jsonify(response), 200
 
+# 列出用戶所有訓練任務的路由
 @training_tasks_bp.route('/list', methods=['GET'])
 @jwt_required
 def list_tasks(user_id):
@@ -71,14 +71,74 @@ def list_tasks(user_id):
         response['msg'] = str(e)
     return jsonify(response), 200
 
+# 查詢特定訓練任務詳情的路由
 @training_tasks_bp.route('/list/<task_id>', methods=['GET'])
 @jwt_required
 def get_task_details(user_id, task_id):
     response = {'code': 200, 'msg': 'ok', 'show_msg': 'ok', 'results': []}
     try:
-        task_list = TrainingTaskService.get_task_by_id(task_id)
-        response['results'] = task_list
+        training_task_id = redis_client.get(task_id)
+        task_detail = TrainingTaskService.get_task_by_id(task_id)
+        task_state = None
+        if training_task_id:
+            task_state = TrainingTaskService.task_training_status_by_object(training_task_id, task_detail)
+        response['results'] = dict(
+            task_detail=task_detail,
+            task_state=task_state
+        )
     except Exception as e:
         response['code'] = 500
         response['msg'] = str(e)
+        traceback.print_exc()
+    try:
+        return jsonify(response), 200
+    except:
+        response['results']['task_state']['data'] = str(response['results']['task_state']['data'])
+        return jsonify(response), 200
+
+# 開始訓練任務的路由
+@training_tasks_bp.route('/train/<task_id>', methods=['POST'])
+@jwt_required
+def train(user_id, task_id):
+    response = {'code': 200, 'msg': 'ok', 'show_msg': 'ok', 'results': None}
+    try:
+        training_task_id = TrainingTaskService.train(task_id)
+        redis_client.set(task_id, str(training_task_id))
+        response['results'] = str(training_task_id)
+    except Exception as e:
+        response['code'] = 500
+        response['msg'] = str(e)
+        traceback.print_exc()
+    return jsonify(response), 200
+
+# 查詢訓練進度的路由
+@training_tasks_bp.route('/train-status/<task_id>/<algo_name>/<framework_name>/<save_key>', methods=['GET'])
+@jwt_required
+def get_train_status(user_id, task_id, algo_name, framework_name, save_key):
+    response = {'code': 200, 'msg': 'ok', 'show_msg': 'ok', 'results': None}
+    try:
+        training_task_id = redis_client.get(task_id)
+        status = TrainingTaskService.task_training_status(training_task_id, algo_name, framework_name, save_key, user_id)
+        response['results'] = status
+        TrainingTaskService.update_training_task_status(task_id, status['state'])
+        redis_client.set(f'{task_id}__status', str(status['state']))
+    except Exception as e:
+        response['code'] = 500
+        response['msg'] = str(e)
+        traceback.print_exc()
+    return jsonify(response), 200
+
+# 停止訓練任務的路由
+@training_tasks_bp.route('/train/<task_id>/<algo_name>/<framework_name>', methods=['DELETE'])
+@jwt_required
+def stop_training(user_id, task_id, algo_name, framework_name):
+    response = {'code': 200, 'msg': '已送出終止信號，任務可能不會立即停止', 'show_msg': 'ok', 'results': None}
+    try:
+        training_task_id = redis_client.get(task_id)
+        TrainingTaskService.stop_training(training_task_id.decode(), algo_name, framework_name)
+        TrainingTaskService.update_training_task_status(task_id, 'REVOKED')
+    except Exception as e:
+        response['code'] = 500
+        response['msg'] = str(e)
+        traceback.print_exc()
     return jsonify(response), 200
