15598149ccada6a9ba0a678503276ab736b26384
Will Cheng Yong <chengyong@pku.edu.cn>
2024-08-18 20:16:55 +0800

    docs: Update Celery worker commands and add notes on pool usage
    - Added recommended Celery worker command using `--concurrency=1` instead of `--pool=solo`.
    - Documented the issues with using `--pool=solo`, including the inability to kill processes and the failure to release GPU memory after training.
    - Provided example commands for running Celery workers with the correct configurations.

--- start_celery.sh
+++ start_celery.sh
@@ -1,1 +1,4 @@
-celery -A run.celery worker --loglevel=info -E --pool solo
+celery -A run.celery worker --loglevel=info -E --concurrency=1
+
+# celery -A run.celery worker --loglevel=info -E --pool solo
+# 不要使用--pool=solo，1. 進程殺不掉 2. 訓練後顯存無法釋放
