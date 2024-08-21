bd3cb0fa4d547ebfa81e00b8b9398d0c231285e6
Will Cheng <will.cheng@efctw.com>
2024-08-21 16:52:51 +0800

    feat: add utility functions to retrieve metrics file and evaluator based on algorithm and framework
    - Introduced the `get_metrics_file` function to dynamically generate the path for the evaluation metrics file based on the algorithm and framework names.
      - Handles paths for YOLOv8 object detection and Detectron2 instance segmentation frameworks.
    - Updated the `get_evaluator` function to return the appropriate evaluator function for starting evaluation tasks based on the algorithm and framework names.
      - Now supports YOLOv8 and Detectron2 frameworks with object detection and instance segmentation.
    - Included error handling for unsupported algorithm and framework combinations to raise a `NotImplementedError`.

--- utils/evaluation_task/__init__.py
+++ utils/evaluation_task/__init__.py
@@ -1,3 +1,4 @@
+import os
 from utils.evaluation_task.evaluation_yolov8_detection import start_eval_yolov8_detection_task
 from utils.evaluation_task.evaluation_detectron2_segmentation import start_eval_detectron2_instance_segmentation_task
 
@@ -7,6 +8,21 @@ OBJECT_DETECTION = 'ObjectDetection'
 INSTANCE_SEGMENTATION = 'InstanceSegmentation'
 YOLOV8 = 'YOLOv8'
 DETECTRON2_INSTANCE_SEGMENTATION = 'Detectron2-InstanceSegmentation'
+
+
+def get_metrics_file(algo_name: str, framework_name: str, training_project_root: str, user_id: str, save_key: str) -> str:
+    project_root = os.path.join(training_project_root, user_id, save_key)
+    if algo_name == OBJECT_DETECTION and framework_name == YOLOV8:
+        training_dir = os.path.join(project_root, 'project', 'exp')
+        metrics_file = os.path.join(training_dir, 'evaluation.json')
+        return metrics_file
+    elif algo_name == INSTANCE_SEGMENTATION and framework_name == DETECTRON2_INSTANCE_SEGMENTATION:
+        training_dir = os.path.join(project_root, 'project')
+        metrics_file = os.path.join(training_dir, 'evaluation.json')
+        return metrics_file
+    else:
+        raise NotImplementedError(f"Metrics Files not implemented for algorithm: {algo_name}, framework: {framework_name}")
+
 
 def get_evaluator(algo_name: str, framework_name: str):
     """
