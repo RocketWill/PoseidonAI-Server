d926976415724584860b9bbb22b8b1f46d03ca7b
Will Cheng <will.cheng@efctw.com>
2024-10-04 17:06:10 +0800

    feat: add Detectron2 instance segmentation visualization task
    - Added integration for Detectron2 instance segmentation visualization
    - Updated `get_visualized_file` to handle Detectron2 instance segmentation
    - Modified `get_visualizer` to support Detectron2 segmentation task initialization
    - Ensured compatibility with YOLOv8 for object detection visualization

--- utils/visualize_val/__init__.py
+++ utils/visualize_val/__init__.py
@@ -1,8 +1,8 @@
 '''
 Author: Will Cheng chengyong@pku.edu.cn
 Date: 2024-08-25 14:44:25
-LastEditors: Will Cheng chengyong@pku.edu.cn
-LastEditTime: 2024-08-25 15:29:43
+LastEditors: Will Cheng (will.cheng@efctw.com)
+LastEditTime: 2024-10-04 11:13:09
 FilePath: /PoseidonAI-Server/utils/visualize_val/__init__.py
 Description: 
 
@@ -10,6 +10,7 @@ Copyright (c) 2024 by chengyong@pku.edu.cn, All Rights Reserved.
 '''
 import os
 from utils.visualize_val.visualize_yolov8_detection import start_visualize_yolov8_detection_task
+from utils.visualize_val.visualize_detectron2_segmentation import start_visualize_detectron2_segmentation_task
 
 
 # Define constants for algorithm and framework names
@@ -23,10 +24,12 @@ def get_visualized_file(algo_name: str, framework_name: str, training_project_ro
     project_root = os.path.join(training_project_root, user_id, save_key)
     if algo_name == OBJECT_DETECTION and framework_name == YOLOV8:
         training_dir = os.path.join(project_root, 'project', 'exp')
-        metrics_file = os.path.join(training_dir, 'visualized.json')
-        return metrics_file
+        vis_file = os.path.join(training_dir, 'visualized.json')
+        return vis_file
     elif algo_name == INSTANCE_SEGMENTATION and framework_name == DETECTRON2_INSTANCE_SEGMENTATION:
-        return NotImplementedError
+        training_dir = os.path.join(project_root, 'project')
+        vis_file = os.path.join(training_dir, 'visualized.json')
+        return vis_file
     else:
         raise NotImplementedError(f"Visualized Files not implemented for algorithm: {algo_name}, framework: {framework_name}")
 
@@ -34,6 +37,6 @@ def get_visualizer(algo_name: str, framework_name: str):
     if algo_name == OBJECT_DETECTION and framework_name == YOLOV8:
         return start_visualize_yolov8_detection_task
     elif algo_name == INSTANCE_SEGMENTATION and framework_name == DETECTRON2_INSTANCE_SEGMENTATION:
-        return NotImplementedError
+        return start_visualize_detectron2_segmentation_task
     else:
         raise NotImplementedError(f"Visualizer not implemented for algorithm: {algo_name}, framework: {framework_name}")
