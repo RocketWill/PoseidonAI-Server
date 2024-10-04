736c2295c4a5d1db4222e9201162285d77a53165
Will Cheng <will.cheng@efctw.com>
2024-10-04 17:06:57 +0800

    chore: add Detectron2 TorchScript dependencies constant
    - Updated constants to include TorchScript dependencies for Detectron2 segmentation
    - Added `DETECTRON2_SEGNET_TS_DLL` for Detectron2 segmentation TorchScript DLL
    - Ensured backward compatibility with YOLOv8 constants

--- utils/export_model/constant.py
+++ utils/export_model/constant.py
@@ -2,7 +2,7 @@
 Author: Will Cheng (will.cheng@efctw.com)
 Date: 2024-09-18 17:11:30
 LastEditors: Will Cheng (will.cheng@efctw.com)
-LastEditTime: 2024-09-18 18:14:46
+LastEditTime: 2024-10-04 14:30:20
 FilePath: /PoseidonAI-Server/utils/export_model/constant.py
 '''
 import os
@@ -15,3 +15,4 @@ current_data_dir = os.path.join(current_dir, 'data')
 
 TORCHSCRIPT_DEPS_DIR = os.path.join(current_data_dir, 'torchscript_deps')
 YOLOV8_DETNET_TS_DLL = os.path.join(current_data_dir, 'yolov8_torchscript', 'EFC_DETNET_TS_runtime.dll')
+DETECTRON2_SEGNET_TS_DLL = os.path.join(current_data_dir, 'detectron2_seg_torchscript', 'EFC_SEGNET_TS_runtime.dll')
