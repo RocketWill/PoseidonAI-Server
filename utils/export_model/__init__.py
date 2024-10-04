22da2ce90569de02b965499ce41ebe7a513e23fe
Will Cheng <will.cheng@efctw.com>
2024-10-04 17:07:26 +0800

    feat: add Detectron2 model export functionality
    - Integrated Detectron2 model export capability in `get_model_exporter` function
    - Updated support for Detectron2 instance segmentation alongside YOLOv8
    - Added import for `start_export_detectron2_model` to handle Detectron2 model export tasks

--- utils/export_model/__init__.py
+++ utils/export_model/__init__.py
@@ -2,7 +2,7 @@
 Author: Will Cheng (will.cheng@efctw.com)
 Date: 2024-09-11 15:51:10
 LastEditors: Will Cheng (will.cheng@efctw.com)
-LastEditTime: 2024-09-18 16:46:54
+LastEditTime: 2024-10-04 14:34:02
 FilePath: /PoseidonAI-Server/utils/export_model/__init__.py
 '''
 import os
@@ -10,6 +10,7 @@ import os
 from os.path import abspath, dirname
 
 from utils.export_model.export_yolov8 import start_export_model as start_export_yolov8_model
+from utils.export_model.export_detectron2 import start_export_model as start_export_detectron2_model
 
 current_file_path = abspath(__file__)
 current_dir = dirname(current_file_path)
@@ -27,14 +28,11 @@ ONNX = 'Onnx'
 OPENVINO = 'OpenVINO'
 NCNN = 'ncnn'
 
-# Define deps directory
-TORCHSCRIPT_DEPS_DIR = os.path.join(current_data_dir, 'torchscript_deps')
-YOLOV8_DETNET_TS_DLL = os.path.join('yolov8_torchscript', 'EFC_DETNET_TS_runtime.dll')
 
 def get_model_exporter(algo_name: str, framework_name: str):
     if algo_name == OBJECT_DETECTION and framework_name == YOLOV8:
         return start_export_yolov8_model
     elif algo_name == INSTANCE_SEGMENTATION and framework_name == DETECTRON2_INSTANCE_SEGMENTATION:
-        raise NotImplementedError('Detectron2 coming soon!')
+        return start_export_detectron2_model
     else:
         raise NotImplementedError(f"Metrics Files not implemented for algorithm: {algo_name}, framework: {framework_name}")
